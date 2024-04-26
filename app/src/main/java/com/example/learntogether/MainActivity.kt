7e35a862793724d53b9b21699628881eea6d309d
Sudheer <21117963+sd4483@users.noreply.github.com>
2024-04-26 23:07:21 +0100

    Add alignment and padding parameters to Column layout

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +35,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TutorialText(title: String, content: String, modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(8.dp)
+    ) {
         Text(
             text = title,
             modifier = modifier
